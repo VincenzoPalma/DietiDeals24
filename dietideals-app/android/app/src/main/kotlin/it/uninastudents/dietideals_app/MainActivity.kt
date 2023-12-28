4a25da0461a0d9ed42e6e811c869d6faf490ad64
VincenzoPalma <vincenzo.palma89@gmail.com>
2023-12-28 15:14:40 +0100

    First commit

---
+++ dietideals-app/android/app/src/main/kotlin/it/uninastudents/dietideals_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package it.uninastudents.dietideals_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ dietideals-service/src/main/java/it/uninastudents/dietidealsservice/DietidealsServiceApplication.java
@@ -0,0 +1,13 @@
+package it.uninastudents.dietidealsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietidealsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietidealsServiceApplication.class, args);
+	}
+
+}
