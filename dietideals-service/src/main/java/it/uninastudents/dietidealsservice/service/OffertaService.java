a047bf2e0c52a08afe06518b5b1b5d66c7baf3b4
Redlux7 <giovanni.paone31@gmail.com>
2024-01-25 13:40:49 +0100

    Implementazione service

---
+++ dietideals-service/src/main/java/it/uninastudents/dietidealsservice/service/OffertaService.java
@@ -0,0 +1,8 @@
+package it.uninastudents.dietidealsservice.service;
+
+import jakarta.persistence.EmbeddedId;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OffertaService {
+}
---
+++ dietideals-service/src/main/java/it/uninastudents/dietidealsservice/service/UtenteService.java
@@ -0,0 +1,7 @@
+package it.uninastudents.dietidealsservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UtenteService {
+}
