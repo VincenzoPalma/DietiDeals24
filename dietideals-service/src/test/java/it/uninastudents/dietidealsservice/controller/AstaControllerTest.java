db552a2292d5dec03790ff2eb2248f616d645159
VincenzoPalma <vincenzo.palma89@gmail.com>
2024-01-30 16:36:29 +0100

    Sviluppo AstaControllerTest.

--- dietideals-service/src/test/java/it/uninastudents/dietidealsservice/controller/AstaControllerTest.java
+++ dietideals-service/src/test/java/it/uninastudents/dietidealsservice/controller/AstaControllerTest.java
@@ -3,8 +3,10 @@ package it.uninastudents.dietidealsservice.controller;
 import io.github.benas.randombeans.EnhancedRandomBuilder;
 import it.uninastudents.dietidealsservice.model.entity.Asta;
 import it.uninastudents.dietidealsservice.model.entity.enums.CategoriaAsta;
+import it.uninastudents.dietidealsservice.model.entity.enums.StatoAsta;
 import it.uninastudents.dietidealsservice.model.entity.enums.TipoAsta;
 import it.uninastudents.dietidealsservice.service.AstaService;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.mockito.InjectMocks;
@@ -17,11 +19,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 import java.util.ArrayList;
 
-import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.Mockito.when;
 
 @WebMvcTest(AstaController.class)
@@ -39,34 +42,58 @@ class AstaControllerTest {
 
     @Test
     void getAsteTest() throws Exception {
-        //Caso in cui la lista dei risultati è vuota
+        //Caso in cui la lista dei risultati è vuota e sono presenti tutti i parametri
         getAsteTestAux(0, "stringa test non trovata", TipoAsta.INGLESE, CategoriaAsta.ALIMENTARI);
-        //Caso in cui la lista dei risultati non è vuota
+        //Caso in cui la lista dei risultati non è vuota e sono presenti tutti i parametri
         getAsteTestAux(3, "stringa test", TipoAsta.INVERSA, CategoriaAsta.ATTREZZI_E_UTENSILI);
+        //Caso in cui la lista dei risultati non è vuota ed è presente solo il parametro stringaRicerca
+        getAsteTestAux(5, "stringa test", null, null);
+        //Caso in cui la lista dei risultati non è vuota ed è presente solo il parametro tipoAsta
+        getAsteTestAux(8, null, TipoAsta.INGLESE, null);
+        //Caso in cui la lista dei risultati non è vuota ed è presente solo il parametro categoriaAsta
+        getAsteTestAux(2, null, null, CategoriaAsta.ARREDAMENTO);
+        //Caso in cui la lista dei risultati non è vuota e non è presente alcun parametro
+        getAsteTestAux(11, null, null, null);
     }
 
     private void getAsteTestAux(int sizeRisultato, String stringaRicerca, TipoAsta tipoAsta, CategoriaAsta categoriaAsta) throws Exception {
         Pageable pageable = PageRequest.of(0, 12);
         ArrayList<Asta> listaAste = new ArrayList<>();
-        for (int i = 0; i < sizeRisultato; i++){
+        for (int i = 0; i < sizeRisultato; i++) {
             Asta nuovaAsta = EnhancedRandomBuilder.aNewEnhancedRandom().nextObject(Asta.class);
+            nuovaAsta.setCategoria(categoriaAsta);
+            nuovaAsta.setStato(StatoAsta.ATTIVA);
+            nuovaAsta.setTipo(tipoAsta);
+            if (stringaRicerca != null) {
+                nuovaAsta.setNome("prova" + stringaRicerca + "test");
+            } else {
+                nuovaAsta.setNome("prova test");
+            }
             listaAste.add(nuovaAsta);
         }
-        Page<Asta> risultati = new PageImpl<>(listaAste, pageable, listaAste.size());
+        Page<Asta> risultati = new PageImpl<>(listaAste, pageable, sizeRisultato);
         when(astaServiceMock.getAll(pageable, stringaRicerca, tipoAsta, categoriaAsta)).thenReturn(risultati);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("asta/ricerca")
-                        .param("page", "0")
-                        .param("size", "12")
-                        .param("nome", stringaRicerca)
-                        .param("tipo", String.valueOf(tipoAsta))
-                        .param("categoria", String.valueOf(categoriaAsta))
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/asta/ricerca")
+                .param("page", "0")
+                .param("size", "12");
+        if (stringaRicerca != null) {
+            requestBuilder.param("nome", stringaRicerca);
+        }
+        if (categoriaAsta != null) {
+            requestBuilder.param("categoria", String.valueOf(categoriaAsta));
+        }
+        if (tipoAsta != null) {
+            requestBuilder.param("tipo", String.valueOf(tipoAsta));
+        }
+        mockMvc.perform(requestBuilder
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.content").isArray())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.content.length()").value(sizeRisultato))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.content[*].nome").value(contains(stringaRicerca)))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.content[*].tipo").value(tipoAsta))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.content[*].categoria").value(categoriaAsta));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[*].nome").value(stringaRicerca != null ? Matchers.everyItem(Matchers.containsString(stringaRicerca)) : Matchers.anything()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[*].tipo").value(tipoAsta != null ? Matchers.everyItem(Matchers.is(tipoAsta)) : Matchers.anything()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[*].stato").value("attivo"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[*].categoria").value(categoriaAsta != null ? Matchers.everyItem(Matchers.is(categoriaAsta)) : Matchers.anything()));
     }
 }
--- dietideals-service/src/test/java/it/uninastudents/dietidealsservice/service/AstaServiceTest.java
+++ dietideals-service/src/test/java/it/uninastudents/dietidealsservice/service/AstaServiceTest.java
@@ -4,10 +4,8 @@ import it.uninastudents.dietidealsservice.repository.AstaRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.mockito.InjectMocks;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
@@ -20,7 +18,7 @@ class AstaServiceTest {
     private AstaService astaService;
 
     @Test
-    void getAllTest(){
+    void getAllTest() {
 
     }
 }
