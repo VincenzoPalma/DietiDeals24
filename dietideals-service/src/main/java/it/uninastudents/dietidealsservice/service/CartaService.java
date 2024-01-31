d8ac0e1a0b4944b85bfc5ba4d7b9df0291e1ff9d
VincenzoPalma <vincenzo.palma89@gmail.com>
2024-01-31 15:16:15 +0100

    Sviluppo Carta Service.

--- dietideals-service/src/main/java/it/uninastudents/dietidealsservice/service/CartaService.java
+++ dietideals-service/src/main/java/it/uninastudents/dietidealsservice/service/CartaService.java
@@ -4,8 +4,6 @@ import it.uninastudents.dietidealsservice.model.entity.Carta;
 import it.uninastudents.dietidealsservice.repository.CartaRepository;
 import it.uninastudents.dietidealsservice.repository.specs.CartaSpecs;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
