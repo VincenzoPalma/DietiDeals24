ea71962409bdd97bf22614907eb2433969789723
Vincenzo Pio Palma <91828151+VincenzoPalma@users.noreply.github.com>
2024-02-21 12:28:48 +0100

    Update IdEntity.java

--- dietideals-service/src/main/java/it/uninastudents/dietidealsservice/model/base/IdEntity.java
+++ dietideals-service/src/main/java/it/uninastudents/dietidealsservice/model/base/IdEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UuidGenerator;
 
